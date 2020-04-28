3c0214f8718b49b7825d818f6465cc22e9b8f00f
andergcp <andergcp@hotmail.com>
2020-04-28 19:21:44 +0000

    [FIX] Fixing result for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     number1 = number * -1
+    a = number1 % 10 * -1
 else:
     number1 = number
-a = number1 % 10
+    a = number1 % 10
 if a > 5:
     b = "and is greater than 5"
 elif a == 0:
