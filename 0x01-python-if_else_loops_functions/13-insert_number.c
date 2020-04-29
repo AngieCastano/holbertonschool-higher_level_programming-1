6019875ca029b2e03afb163b78f8a811edd863a1
andergcp <andergcp@hotmail.com>
2020-04-29 00:36:40 +0000

    [FIX] Adding edge cases to 13 task, add node to the beginning and end of linked list.

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,19 +13,24 @@ listint_t *insert_node(listint_t **head, int number)
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
-	new->n = number;
-	tmp = *head;
-
+	new->n = number, tmp = *head;
 	for (i = 0; tmp; i++)
 	{
 		if (number > tmp->n)
 		{
+			if (!tmp->next)
+				return (tmp->next = new, new->next = NULL, new);
 			tmp = tmp->next;
 			continue;
 		}
 		else
 		{
 			new->next = tmp;
+			if (tmp == *head)
+			{
+				*head = new;
+				break;
+			}
 			tmp = *head;
 			for (j = 0; j < i; j++)
 			{
