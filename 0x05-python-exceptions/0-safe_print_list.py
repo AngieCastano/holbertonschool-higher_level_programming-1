89b100f28b9f1bc9b1866a1362a6ad2963e68e44
andergcp <andergcp@hotmail.com>
2020-05-18 16:53:04 +0000

    [TASK] Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        counter = 0
+        for i in my_list:
+            if counter < x:
+                print("{}".format(i), end="")
+                counter += 1
+        print("")
+    except TypeError:
+        print("No right type")
+    finally:
+        return counter
