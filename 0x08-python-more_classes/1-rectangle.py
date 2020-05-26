274cafad94e9bf304fe7fffb02bab2c8574cb0e1
andergcp <andergcp@hotmail.com>
2020-05-26 00:44:37 +0000

    [FIX] Fixing documentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-class Rectangle:
+'''Classes and objects project'''
 
+
+class Rectangle:
+    '''class Rectangle'''
     def __init__(self, width=0, height=0):
+        '''Initializes the instance'''
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        '''Property to asign value to width'''
         return self.__width
 
     @width.setter
     def width(self, value):
+        '''width setter'''
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -19,10 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
+        '''Property to asign value to height'''
         return self.__height
 
     @height.setter
     def height(self, value):
+        '''height setter'''
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
