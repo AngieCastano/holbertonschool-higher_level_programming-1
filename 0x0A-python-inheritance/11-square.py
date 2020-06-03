78c0475f630c1fd992f3e5d0790a337552f98eb6
andergcp <andergcp@hotmail.com>
2020-06-03 03:36:17 +0000

    [FIX] Adding integer validator method to init

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,6 +9,7 @@ class Square(Rectangle):
     '''Class of a square inherited from Rectangle class'''
     def __init__(self, size):
         '''Instantiates square based on rectangle and adding size'''
+        self.integer_validator('size', size)
         self.__size = size
         super().__init__(self.__size, self.__size)
 
