dd25ca24841e889367737e01597e8e9d4ff372b3
andergcp <andergcp@hotmail.com>
2020-06-03 02:31:08 +0000

    [FIX] Documentation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''Python in Holberton'''
+
+
 class MyList(list):
+    '''Class inherited from list'''
     def print_sorted(self):
+        '''prints the lists in ascending order'''
         print(sorted(self))
