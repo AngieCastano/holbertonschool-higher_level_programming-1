6832a8a4001375dd25888a5e68312939b6c748b0
andergcp <andergcp@hotmail.com>
2020-06-03 03:07:56 +0000

    [FIX] Documentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+''' Python in Holberton '''
+
+
 def is_same_class(obj, a_class):
+    ''' returns True if the object is exactly
+    an instance of the specified class ;
+    otherwise False.'''
+
     if dir(obj) == dir(a_class):
         return True
     return False
