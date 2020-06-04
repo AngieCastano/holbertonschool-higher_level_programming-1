c9d75fbf13bd5d1691ce5d47b906e323162fa50d
andergcp <andergcp@hotmail.com>
2020-06-04 00:08:42 +0000

    [FIX] Putting documentation in line 2

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-
 '''
 I/O Python
 Adds all arguments to a Python list,
 and then save them to a file
 '''
+
+from sys import argv
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 
 try:
