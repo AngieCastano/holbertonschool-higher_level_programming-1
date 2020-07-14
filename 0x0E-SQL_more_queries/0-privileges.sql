373c915e33f2b4d43694f9a61e4297a33873eb22
andergcp <andergcp@hotmail.com>
2020-07-14 17:15:38 +0000

    [FIX] Separating statements

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Lists all databases of the your MySQL serverlists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 -- Done by andergcp
-SHOW GRANTS FOR 'user_0d_1'@'localhost','user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
