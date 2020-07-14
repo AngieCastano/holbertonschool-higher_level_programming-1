8a74d822495b4bae4cd52a2f9587307c01f44c54
andergcp <andergcp@hotmail.com>
2020-07-14 16:51:01 +0000

    [TASK] Task 0

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- Lists all databases of the your MySQL serverlists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- Done by andergcp
+SHOW GRANTS FOR 'user_0d_1'@'localhost','user_0d_2'@'localhost';
