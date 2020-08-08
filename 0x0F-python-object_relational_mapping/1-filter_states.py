e6c48e0853f290748b5f4db9b8168399c38bb0ba
andergcp <andergcp@hotmail.com>
2020-08-08 14:02:59 +0000

    [FIX] Didn't validate upper case, added BINARY before N%

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''
-lists all states from the database hbtn_0e_0_usa
+lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa
 '''
 import sys
 import MySQLdb
@@ -10,10 +11,15 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(
         host="localhost", port=3306, user=sys.argv[1],
         passwd=sys.argv[2], db=sys.argv[3])
+
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+
     query_rows = cur.fetchall()
+
     for row in query_rows:
         print(row)
+
     cur.close()
     conn.close()
