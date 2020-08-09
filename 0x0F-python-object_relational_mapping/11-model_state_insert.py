208ed5c2ada0a34bbffbca641dd69dc1ec176e48
andergcp <andergcp@hotmail.com>
2020-08-09 11:45:02 +0000

    [TASK] Task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+adds the State object “Louisiana” to the database
+'''
+from model_state import Base, State
+import sys
+import sqlalchemy as sa
+from sqlalchemy import (create_engine)
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    conn = engine.connect()
+    metadata = sa.MetaData()
+
+    states = sa.Table('states', metadata, autoload=True, autoload_with=engine)
+    query = sa.insert(states).values(name='Louisiana')
+    query = sa.select(states)
+
+    ResultProxy = conn.execute(query)
+    ResultSet = ResultProxy.fetchall()
+
+    for key, value in ResultSet:
+        print("{}: {}".format(key, value))
