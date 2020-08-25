1f803a9b09da3ad2a8bf6d56024fee5a6252105a
andergcp <andergcp@hotmail.com>
2020-08-25 00:46:07 +0000

    [TASK] Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import urllib.request
+
+if __name__ == '__main__':
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        r = response.read()
+        print('Body response:')
+        print('\t- type:',type(r))
+        print('\t- content:', r)
+        print('\t- utf8 content:', r.decode('utf-8'))
