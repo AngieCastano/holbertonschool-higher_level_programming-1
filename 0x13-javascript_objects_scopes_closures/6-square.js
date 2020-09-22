c1f82b836e4d7ca53bbad8c0a6f00163c3eedbcb
andergcp <andergcp@hotmail.com>
2020-09-22 02:13:13 +0000

    [FIX] Changing literal for variable

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const OldSquare = require('./5-square');
+
 class Square extends OldSquare {
   charPrint (c) {
     let l = 'X';
     if (c !== undefined) {
-      l = 'C';
+      l = c;
     }
     for (let i = 0; i < this.height; i++) {
       console.log(l.repeat(this.width));
