b5f675aa2f69ad0ef88b44f145c679a3cd55409e
andergcp <andergcp@hotmail.com>
2020-09-23 02:39:24 +0000

    [TASK] Task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const URL = process.argv[2];
+const request = require('request');
+const characterID = '18';
+const characterURL = 'https://swapi-api.hbtn.io/api/people/' + characterID + '/';
+let counter = 0;
+
+request(URL, function (err, response, body) {
+  const jsonObj = JSON.parse(body);
+  const movies = jsonObj.results;
+  for (let i = 0; i < movies.length; i++) {
+    const characters = movies[i].characters;
+    if (characters.includes(characterURL)) {
+      counter += 1;
+    }
+  }
+  console.log(counter);
+  if (err) {}
+});
